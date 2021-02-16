ec513749ebf2cebc8cc280a9d1136ae8889fb740
Сергей Лабузов <sergeylabuzav@gmail.com>
2021-02-16 18:23:00 +0300

    paw-1: Add Google provider.

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,29 +1,28 @@
-import React, { useRef, useState } from "react"
-import { Form, Button, Card, Alert } from "react-bootstrap"
-import { useAuth } from "../contexts/AuthContext"
-import { Link } from "react-router-dom"
+import React, { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import { Alert, Button, Card, Form } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function ForgotPassword() {
-  const emailRef = useRef()
-  const { resetPassword } = useAuth()
-  const [error, setError] = useState("")
-  const [message, setMessage] = useState("")
-  const [loading, setLoading] = useState(false)
+  const emailRef = useRef();
+  const { resetPassword } = useAuth();
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
   async function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     try {
-      setMessage("")
-      setError("")
-      setLoading(true)
-      await resetPassword(emailRef.current.value)
-      setMessage("Check your inbox for further instructions")
+      setMessage("");
+      setError("");
+      setLoading(true);
+      await resetPassword(emailRef.current.value);
+      setMessage("Check your inbox for further instructions");
     } catch {
-      setError("Failed to reset password")
+      setError("Failed to reset password");
     }
-
-    setLoading(false)
+    setLoading(false);
   }
 
   return (
@@ -31,14 +30,14 @@ export default function ForgotPassword() {
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Password Reset</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          {message && <Alert variant="success">{message}</Alert>}
-          <Form onSubmit={handleSubmit}>
+          { error && <Alert variant="danger">{ error }</Alert> }
+          { message && <Alert variant="success">{ message }</Alert> }
+          <Form onSubmit={ handleSubmit }>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
+              <Form.Control type="email" ref={ emailRef } required/>
             </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
+            <Button disabled={ loading } className="w-100" type="submit">
               Reset Password
             </Button>
           </Form>
